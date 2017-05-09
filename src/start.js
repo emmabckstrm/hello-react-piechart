fe6bc912ba88fbd28af1d6d8d06bd548aaa576aa
Johan Bengtsson <jqb@kth.se>
2017-05-09 14:02:08 +0200

    initial commit

---
+++ src/start.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+// require('../style/main.less');
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
