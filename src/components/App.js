ff745cbacad05a644e7e27ada07f9861694509e9
Johan Bengtsson <jqb@kth.se>
2017-05-10 11:38:57 +0200

    added react-d3 and sample pie chart

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
 import React, {Component} from 'react'
+const PieChart = require('react-d3/piechart').PieChart;
 
 export default class App extends Component {
 
-    render() {
+    render () {
+        const pieData =  [
+          {label: 'Margarita', value: 20.0},
+          {label: 'John', value: 55.0},
+          {label: 'Tim', value: 25.0 }
+        ];
         return (
-            <h1>Hello world!</h1>
+          <div>
+            <h1>Hello PieChart!</h1>
+            <PieChart
+              data={pieData}
+              width={400}
+              height={400}
+              radius={100}
+              innerRadius={20}
+              title="This is my Pie Chart"
+            />
+        </div>
         );
     }
 }
